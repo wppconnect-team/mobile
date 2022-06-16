f637f97d978427ff20414d099cc7def4d471045c
João Victor Souza <souza.joao1997@gmail.com>
2022-06-16 01:57:01 -0300

    feat: starting project mobile with rn

---
+++ src/App.js
@@ -0,0 +1,152 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  BackHandler,
+  useWindowDimensions,
+  Linking,
+  View
+} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+import {
+  FlingGestureHandler,
+  Directions,
+  State,
+} from 'react-native-gesture-handler';
+import WebView from 'react-native-webview';
+import axios from 'axios';
+
+const userAgent = `Mozilla/5.0 (Linux; Win64; x64; rv:46.0) Gecko/20100101 Firefox/68.0`;
+const INJECTED_JAVASCRIPT = `
+	(function () {
+		// Disable zooming in (textinput focus zoom messes up ux)
+		const meta = document.createElement('meta');
+		meta.setAttribute(
+			'content',
+			'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+		);
+		meta.setAttribute('name', 'viewport');
+		document.getElementsByTagName('head')[0].appendChild(meta);
+    
+    window.ReactNativeWebView.postMessage(
+      JSON.stringify({ message: 'start'}),
+    );
+	})();
+`;
+const whatsappWebUri = 'https://web.whatsapp.com';
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  const webviewRef = React.useRef();
+  const { height, width } = useWindowDimensions();
+  const loadInjecte = async () => {
+    const wajs = await axios.get(
+      'https://github.com/wppconnect-team/wa-js/releases/download/nightly/wppconnect-wa.js',
+    );
+    if (wajs.status === 200 && wajs.data) {
+      webviewRef.current?.injectJavaScript(`	
+        ${wajs.data}
+        WPP.webpack.onReady(function () {
+          window.ReactNativeWebView.postMessage(
+            JSON.stringify({ message: 'Ready to use WPPConnect WA-JS'}),
+          );
+        });
+      `);
+    }
+  }
+  const onMessage = React.useCallback((event) => {
+    let data = JSON.parse(event.nativeEvent.data);
+    console.log(data);
+    if(data?.message === 'start') {
+      loadInjecte();
+    }
+  }, []);
+  return (
+    <SafeAreaView style={[styles.container, backgroundStyle]}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <FlingGestureHandler
+        direction={Directions.RIGHT}
+        onHandlerStateChange={({ nativeEvent }) => {
+          if (nativeEvent.state === State.ACTIVE) {
+            webviewRef.current?.injectJavaScript(
+              `	
+							document.querySelector('#main').parentElement.style.display = 'none';
+							document.querySelector('#side').parentElement.style.display = 'block';
+						`,
+            );
+          }
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+          }}
+        >
+          <WebView
+            ref={webviewRef}
+            source={{
+              uri: whatsappWebUri,
+            }}
+            userAgent={userAgent}
+            injectedJavaScript={INJECTED_JAVASCRIPT}
+            onNavigationStateChange={(event) => {
+              if (
+                !event.url
+                  .toLowerCase()
+                  .includes(whatsappWebUri.toLowerCase()) &&
+                event.navigationType === 'click'
+              ) {
+                webviewRef.current?.stopLoading();
+                Linking.openURL(event.url);
+              }
+            }}
+            style={[styles.webView, { height: height, width: width }]}
+            onMessage={onMessage}
+          />
+        </View>
+      </FlingGestureHandler>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  webView: {
+    flex: 1,
+  },
+});
+
+export default App;
